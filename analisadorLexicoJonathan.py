c65dba4bbbad7e2e486b785b74871860480f10f7
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-12-08 16:03:04 -0300

    Add: Agora ele diz em que linha o erro está

--- analisadorLexicoJonathan.py
+++ analisadorLexicoJonathan.py
@@ -2,6 +2,7 @@ import re
 
 TOKENS = [
     {"token": "DECLARACAO_MAIN", "pattern": r"\bmain\b"},
+    {"token": "TIPO_VARIAVEL", "pattern": r"\b(int|float|double|char)\b"},
     {"token": "PARENTESE_ABRE", "pattern": r"\("},
     {"token": "PARENTESE_FECHA", "pattern": r"\)"},
     {"token": "CHAVE_ABRE", "pattern": r"\{"},
@@ -35,9 +36,16 @@ TOKENS = [
 def analisador_lexico(input_string):
     tokens_identificados = []
     index = 0
+    numero_linha = 1  # Inicia a contagem de linhas a partir de 1
 
     while index < len(input_string):
         matched = False
+
+        # Verifica se é uma quebra de linha e atualiza o contador de linhas
+        if input_string[index] == "\n":
+            numero_linha += 1
+            index += 1
+            continue
 
         for token_info in TOKENS:
             pattern = r'^' + token_info["pattern"]
@@ -47,15 +55,48 @@ def analisador_lexico(input_string):
                 matched = True
 
                 if token_info["token"] not in ["ESPACO", "COMENTARIO"]:
-                    tokens_identificados.append({"token": token_info["token"], "value": match.group(0)})
+                    tokens_identificados.append({"token": token_info["token"], "value": match.group(0), "linha": numero_linha})
 
                 index += len(match.group(0))
                 break
 
         if not matched:
-            return f"Caractere inválido na posição {index}: {input_string[index]}"
+            return f"Erro: Caractere inválido na linha {numero_linha}, posição {index}: {input_string[index]}"
 
     return tokens_identificados
+def analisar_declaracao_variavel(tokens):
+    if not tokens:
+        return False
+
+    # Verifica se o primeiro token é um tipo de variável
+    if tokens[0]["token"] != "TIPO_VARIAVEL":
+        return False
+
+    # Deve haver pelo menos um identificador após o tipo
+    tokens = tokens[1:]
+    if not tokens or tokens[0]["token"] != "IDENTIFICADOR":
+        return False
+
+    # Processa o resto dos tokens
+    tokens = tokens[1:]
+    while tokens:
+        # Verifica se há um ponto e vírgula para terminar a declaração
+        if tokens[0]["token"] == "PONTO_VIRGULA":
+            return True
+
+        # Se houver uma vírgula, deve haver outro identificador
+        if tokens[0]["token"] == "VIRGULA":
+            tokens = tokens[1:]
+            if not tokens or tokens[0]["token"] != "IDENTIFICADOR":
+                return False
+        else:
+            # Se não for uma vírgula, não é uma declaração de variável válida
+            return False
+
+        tokens = tokens[1:]
+
+    # Se a lista de tokens terminou sem encontrar um ponto e vírgula, a declaração é inválida
+    return False
 code = '''
 int main() {
     int x, y;
@@ -73,7 +114,7 @@ int main() {
         y = y - 1;
     }
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0@; i < 5; i++) {
         x = x + i;
     }
 
